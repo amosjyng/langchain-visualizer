7a32cf4001a0b8e1cddce3c16cfb88f6c8f28db5
Amos Jun-yeung Ng <me@amos.ng>
2023-01-24 11:54:16 +0700

    Get SERP example fully reproducible and testable

--- tests/agents/test_langchain_getting_started.py
+++ tests/agents/test_langchain_getting_started.py
@@ -1,7 +1,6 @@
 import langchain_visualizer  # isort:skip  # noqa: F401
 import asyncio
 
-import pytest
 from langchain.agents import initialize_agent, load_tools
 from langchain.llms import OpenAI
 
@@ -20,17 +19,18 @@ agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbos
 
 @vcr.use_cassette()
 async def search_agent_demo():
+    # reload tools because otherwise they will not use the cassette
+    agent.tools = load_tools(["serpapi", "llm-math"], llm=llm)
     return agent.run(
         "Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 "
         "power?"
     )
 
 
-@pytest.mark.skip(reason="need vcr-langchain to record tool usage")
 def test_llm_usage_succeeds():
     """Check that the chain can run normally"""
     result = asyncio.get_event_loop().run_until_complete(search_agent_demo())
-    assert result.strip().endswith("raised to the 0.23 power is 2.4001527295633003.")
+    assert result.strip().endswith("raised to the 0.23 power is 2.4242784855673896.")
 
 
 if __name__ == "__main__":
