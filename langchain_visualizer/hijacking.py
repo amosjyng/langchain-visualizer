181eae20f1063660f3c7feaeb31d4cee358fc7d5
Amos Jun-yeung Ng <me@amos.ng>
2023-01-24 18:18:23 +0700

    Visualize cached tooling

--- langchain_visualizer/hijacking.py
+++ langchain_visualizer/hijacking.py
@@ -1,7 +1,13 @@
 import asyncio
 
+import gorilla
 from ice.trace import TracedABC
 from pydantic import Extra
+
+LANGCHAIN_VISUALIZER_PATCH_ID = "lc-viz"
+VCR_LANGCHAIN_PATCH_ID = "lc-vcr"
+# override prefix used by vcr-langchain for visualization compatibility
+VCR_VIZ_INTEROP_PREFIX = "_vcr_"
 
 
 class VisualizationWrapper(TracedABC):
@@ -30,12 +36,21 @@ class PydanticBypass:
             self.obj.__class__.__config__.extra = Extra.forbid
 
 
-def ice_agent_getter(og_fn):
+def ice_agent_getter(og_fn_name: str):
     def get_ice_agent(self):
         # we have to do this roundabout thing because we don't know ahead of time all
         # the classes we want to set ice_agent for
         if not hasattr(self, "ice_agent"):
             with PydanticBypass(self):
+                vcr_key = VCR_VIZ_INTEROP_PREFIX + og_fn_name
+                if hasattr(self.__class__, vcr_key):
+                    # if vcr-langchain is here as well, call them so that caching can
+                    # happen
+                    og_fn = getattr(self.__class__, vcr_key)
+                else:
+                    og_fn = gorilla.get_original_attribute(
+                        self.__class__, og_fn_name, LANGCHAIN_VISUALIZER_PATCH_ID
+                    )
                 self.ice_agent = VisualizationWrapper(self, og_fn=og_fn)
         return self.ice_agent
 
@@ -61,6 +76,20 @@ def hijack(cls, fn_name, get_replacement):
 
 
 def ice_hijack(cls, og_method_name):
-    og_method = getattr(cls, og_method_name)
-    setattr(cls, "get_ice_agent", ice_agent_getter(og_method))
-    setattr(cls, og_method_name, overridden_call)
+    gorilla.apply(
+        gorilla.Patch(
+            destination=cls,
+            name="get_ice_agent",
+            obj=ice_agent_getter(og_method_name),
+            settings=gorilla.Settings(allow_hit=False),
+        )
+    )
+    gorilla.apply(
+        gorilla.Patch(
+            destination=cls,
+            name=og_method_name,
+            obj=overridden_call,
+            settings=gorilla.Settings(store_hit=True, allow_hit=True),
+        ),
+        id=LANGCHAIN_VISUALIZER_PATCH_ID,
+    )
