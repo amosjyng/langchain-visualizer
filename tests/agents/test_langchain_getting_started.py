203a57f6e600294715dfff8585578b48fb43bdf4
Amos Jun-yeung Ng <me@amos.ng>
2023-01-25 14:52:39 +0700

    Simplify agent running test code

--- tests/agents/test_langchain_getting_started.py
+++ tests/agents/test_langchain_getting_started.py
@@ -9,22 +9,18 @@ from tests import vcr
 # ========================== Start of langchain example code ==========================
 # https://langchain.readthedocs.io/en/latest/modules/agents/getting_started.html
 
-llm = OpenAI(temperature=0)
-tools = load_tools(["serpapi", "llm-math"], llm=llm)
-agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
-
-
-# ================================== Execute example ==================================
-
-
 @vcr.use_cassette()
 async def search_agent_demo():
-    # reload tools because otherwise they will not use the cassette
-    agent.tools = load_tools(["serpapi", "llm-math"], llm=llm)
+    llm = OpenAI(temperature=0)
+    tools = load_tools(["serpapi", "llm-math"], llm=llm)
+    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
     return agent.run(
         "Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 "
         "power?"
     )
+
+
+# ================================== Execute example ==================================
 
 
 def test_llm_usage_succeeds():
