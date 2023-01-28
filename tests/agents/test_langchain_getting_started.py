e5df660576d18844cf89d364203fd484ae7b15a1
Amos Jun-yeung Ng <me@amos.ng>
2023-01-28 09:03:11 +0700

    Formatting

--- tests/agents/test_langchain_getting_started.py
+++ tests/agents/test_langchain_getting_started.py
@@ -9,11 +9,14 @@ from tests import vcr
 # ========================== Start of langchain example code ==========================
 # https://langchain.readthedocs.io/en/latest/modules/agents/getting_started.html
 
+
 @vcr.use_cassette()
 async def search_agent_demo():
     llm = OpenAI(temperature=0)
     tools = load_tools(["serpapi", "llm-math"], llm=llm)
-    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+    agent = initialize_agent(
+        tools, llm, agent="zero-shot-react-description", verbose=True
+    )
     return agent.run(
         "Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 "
         "power?"
