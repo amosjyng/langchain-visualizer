bc215fa46eaf1cc06ffe5d749ce5b1d082e4b5ab
Amos Jun-yeung Ng <me@amos.ng>
2023-02-02 20:41:28 +0700

    Visualize Bash calls as well

--- langchain_visualizer/agents/tools.py
+++ langchain_visualizer/agents/tools.py
@@ -1,7 +1,9 @@
 from langchain.python import PythonREPL
 from langchain.serpapi import SerpAPIWrapper
+from langchain.utilities.bash import BashProcess
 
 from langchain_visualizer.hijacking import ice_hijack
 
 ice_hijack(SerpAPIWrapper, "run")
 ice_hijack(PythonREPL, "run")
+ice_hijack(BashProcess, "run")
---
+++ tests/chains/test_bash_chain.py
@@ -0,0 +1,30 @@
+import langchain_visualizer  # isort:skip  # noqa: F401
+import asyncio
+
+from langchain.chains import LLMBashChain
+from langchain.llms import OpenAI
+
+from tests import vcr
+
+llm = OpenAI(temperature=0)
+chain = LLMBashChain(llm=llm)
+
+
+# ================================== Execute example ==================================
+
+
+@vcr.use_cassette()
+async def bash_chain_demo():
+    return chain("What files are in my current directory?")
+
+
+def test_llm_usage_succeeds():
+    """Check that the chain can run normally"""
+    result = asyncio.get_event_loop().run_until_complete(bash_chain_demo())
+    assert "langchain_visualizer" in result["answer"]
+
+
+if __name__ == "__main__":
+    from langchain_visualizer import visualize
+
+    visualize(bash_chain_demo)
