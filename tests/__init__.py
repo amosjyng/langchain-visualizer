a9af3abd8beaa7556525ad83f72bf89f941863ac
Amos Jun-yeung Ng <me@amos.ng>
2023-02-02 20:41:42 +0700

    Make extra sure no recordings are happening unless specified

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+from vcr.record_mode import RecordMode
 from vcr_langchain import VCR
 
-vcr = VCR(path_transformer=VCR.ensure_suffix(".yaml"))
+vcr = VCR(path_transformer=VCR.ensure_suffix(".yaml"), record_mode=RecordMode.NONE)
