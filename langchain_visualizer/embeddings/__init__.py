f43755a45c3f6d5ad453007138344cd0a27b6ab1
Amos Jun-yeung Ng <me@amos.ng>
2023-02-03 16:21:23 +0700

    Enable hijacking multiple functions on same object

--- langchain_visualizer/embeddings/__init__.py
+++ langchain_visualizer/embeddings/__init__.py
@@ -4,4 +4,5 @@ from langchain_visualizer.hijacking import ice_hijack
 
 
 def visualize_embeddings():
+    ice_hijack(OpenAIEmbeddings, "_embedding_func")
     ice_hijack(OpenAIEmbeddings, "embed_documents")
--- langchain_visualizer/hijacking.py
+++ langchain_visualizer/hijacking.py
@@ -2,7 +2,6 @@ import asyncio
 
 import gorilla
 from ice.trace import TracedABC
-from pydantic import Extra
 
 LANGCHAIN_VISUALIZER_PATCH_ID = "lc-viz"
 VCR_LANGCHAIN_PATCH_ID = "lc-vcr"
@@ -19,55 +18,36 @@ class VisualizationWrapper(TracedABC):
         return self.og_fn(self.og_obj, *args, **kwargs)
 
 
-class PydanticBypass:
-    def __init__(self, obj):
-        self.obj = obj
-        self.has_config = hasattr(self.obj.__class__, "__config__")
-        self.has_extra = self.has_config and hasattr(
-            self.obj.__class__.__config__, "extra"
+def get_viz_wrapper(og_self, og_fn_name: str):
+    vcr_key = VCR_VIZ_INTEROP_PREFIX + og_fn_name
+    if hasattr(og_self.__class__, vcr_key):
+        # if vcr-langchain is here as well, call them so that caching can
+        # happen
+        og_fn = getattr(og_self.__class__, vcr_key)
+    else:
+        og_fn = gorilla.get_original_attribute(
+            og_self.__class__, og_fn_name, LANGCHAIN_VISUALIZER_PATCH_ID
         )
-
-    def __enter__(self):
-        if self.has_extra:
-            self.obj.__class__.__config__.extra = Extra.allow
-
-    def __exit__(self, *args):
-        if self.has_extra:
-            self.obj.__class__.__config__.extra = Extra.forbid
+    return VisualizationWrapper(og_self, og_fn=og_fn)
 
 
-def ice_agent_getter(og_fn_name: str):
-    def get_ice_agent(self):
-        # we have to do this roundabout thing because we don't know ahead of time all
-        # the classes we want to set ice_agent for
-        if not hasattr(self, "ice_agent"):
-            with PydanticBypass(self):
-                vcr_key = VCR_VIZ_INTEROP_PREFIX + og_fn_name
-                if hasattr(self.__class__, vcr_key):
-                    # if vcr-langchain is here as well, call them so that caching can
-                    # happen
-                    og_fn = getattr(self.__class__, vcr_key)
-                else:
-                    og_fn = gorilla.get_original_attribute(
-                        self.__class__, og_fn_name, LANGCHAIN_VISUALIZER_PATCH_ID
-                    )
-                self.ice_agent = VisualizationWrapper(self, og_fn=og_fn)
-        return self.ice_agent
+def get_overridden_call(og_method_name):
+    def overridden_call(og_self, *args, **kwargs):
+        """Preserve sync nature of OG call method"""
+        ice_agent = get_viz_wrapper(og_self, og_method_name)
+        if (
+            not hasattr(og_self.__class__, "_should_trace")
+            or og_self.__class__._should_trace
+        ):
+            # ICE displays class name in visualization
+            ice_agent.__class__.__name__ = og_self.__class__.__name__
+            return asyncio.get_event_loop().run_until_complete(
+                ice_agent.run(*args, **kwargs)
+            )
 
-    return get_ice_agent
+        return ice_agent.og_fn(og_self, *args, **kwargs)
 
-
-def overridden_call(self, *args, **kwargs):
-    """Preserve sync nature of OG call method"""
-    ice_agent = self.get_ice_agent()
-    if not hasattr(self.__class__, "_should_trace") or self.__class__._should_trace:
-        # ICE displays class name in visualization
-        ice_agent.__class__.__name__ = self.__class__.__name__
-        return asyncio.get_event_loop().run_until_complete(
-            ice_agent.run(*args, **kwargs)
-        )
-
-    return ice_agent.og_fn(self, *args, **kwargs)
+    return overridden_call
 
 
 def hijack(cls, fn_name, get_replacement):
@@ -79,16 +59,8 @@ def ice_hijack(cls, og_method_name):
     gorilla.apply(
         gorilla.Patch(
             destination=cls,
-            name="get_ice_agent",
-            obj=ice_agent_getter(og_method_name),
-            settings=gorilla.Settings(allow_hit=False),
-        )
-    )
-    gorilla.apply(
-        gorilla.Patch(
-            destination=cls,
             name=og_method_name,
-            obj=overridden_call,
+            obj=get_overridden_call(og_method_name),
             settings=gorilla.Settings(store_hit=True, allow_hit=True),
         ),
         id=LANGCHAIN_VISUALIZER_PATCH_ID,
