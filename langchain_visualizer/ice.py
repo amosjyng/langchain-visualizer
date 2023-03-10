5b744823827a73817f895287e8a8303ae1bd4671
Amos Jun-yeung Ng <me@amos.ng>
2023-03-10 16:54:18 +1100

    Visualize new Chat API properly

--- langchain_visualizer/ice.py
+++ langchain_visualizer/ice.py
@@ -1,23 +1,49 @@
 from typing import Any, List, Union
 
 from ice import json_value
-from langchain.schema import LLMResult
+from langchain.schema import (
+    AIMessage,
+    ChatResult,
+    HumanMessage,
+    LLMResult,
+    SystemMessage,
+)
 
 og_json_value = json_value.to_json_value
 
 
 def to_json_value(x: Any) -> json_value.JSONValue:
     if isinstance(x, LLMResult):
-        generations = x.generations
-        texts: Union[List[List[str]], List[str], str] = [
-            g.text for sublist in generations for g in sublist
+        regular_generations = x.generations
+        regular_texts: Union[List[List[str]], List[str], str] = [
+            g.text for sublist in regular_generations for g in sublist
         ]
-        if len(texts) == 1:
-            texts = texts[0]
-        if len(texts) == 1:
+        if len(regular_texts) == 1:
+            regular_texts = regular_texts[0]
+        if len(regular_texts) == 1:
             # do it a second time because it's a list of lists
-            texts = texts[0]
-        return og_json_value(texts)
+            regular_texts = regular_texts[0]
+        return og_json_value(regular_texts)
+    elif isinstance(x, ChatResult):
+        chat_generations = x.generations
+        chat_texts: Union[List[str], str] = [
+            chat_generation.text for chat_generation in chat_generations
+        ]
+        if len(chat_texts) == 1:
+            regular_texts = chat_texts[0]
+        return og_json_value(chat_texts)
+    elif isinstance(x, SystemMessage):
+        return {
+            "System": x.content,
+        }
+    elif isinstance(x, AIMessage):
+        return {
+            "AI": x.content,
+        }
+    elif isinstance(x, HumanMessage):
+        return {
+            "Human": x.content,
+        }
 
     return og_json_value(x)
 
--- tests/llms/test_chatgpt.py
+++ tests/llms/test_chatgpt.py
@@ -2,20 +2,43 @@ import langchain_visualizer  # isort:skip  # noqa: F401
 import asyncio
 
 import vcr_langchain as vcr
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
 
 # ========================== Start of langchain example code ==========================
-# https://langchain.readthedocs.io/en/latest/modules/prompts/getting_started.html
-
-
-llm = OpenAI(model_name="gpt-3.5-turbo")
-
-# ================================== Execute example ==================================
+# https://langchain.readthedocs.io/en/latest/modules/chat/getting_started.html
 
 
 @vcr.use_cassette()
 async def chatgpt_demo():
-    return llm("Write me 5 paragraphs on why technological progress is necessary")
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo")
+
+    template = (
+        "You are a helpful assistant that translates {input_language} to "
+        "{output_language}."
+    )
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    # get a chat completion from the formatted messages
+    return chat(
+        chat_prompt.format_prompt(
+            input_language="English",
+            output_language="French",
+            text="I love programming.",
+        ).to_messages()
+    )
+
+
+# ================================== Execute example ==================================
 
 
 def test_llm_usage_succeeds():
@@ -24,7 +47,7 @@ def test_llm_usage_succeeds():
     Also, record playback for easy visualization.
     """
     result = asyncio.get_event_loop().run_until_complete(chatgpt_demo())
-    assert result.endswith("for generations to come.")
+    assert result.content == "J'adore programmer."
 
 
 if __name__ == "__main__":
