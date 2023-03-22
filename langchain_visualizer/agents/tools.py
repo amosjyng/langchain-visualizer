2afb88c5214ff4ed5c5b1906134da91440a72dda
Amos Jun-yeung Ng <me@amos.ng>
2023-03-23 10:46:38 +1100

    Demo the chat version of the MRKL agent

--- langchain_visualizer/agents/tools.py
+++ langchain_visualizer/agents/tools.py
@@ -1,9 +1,11 @@
 from langchain.python import PythonREPL
 from langchain.serpapi import SerpAPIWrapper
 from langchain.utilities.bash import BashProcess
+from langchain.sql_database import SQLDatabase
 
 from langchain_visualizer.hijacking import ice_hijack
 
 ice_hijack(SerpAPIWrapper, "run")
 ice_hijack(PythonREPL, "run")
 ice_hijack(BashProcess, "run")
+ice_hijack(SQLDatabase, "run")
