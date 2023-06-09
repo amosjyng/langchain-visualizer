f3c38e37f1185b1a5e55aaf0c5ec676fed661e10
Amos Jun-yeung Ng <me@amos.ng>
2023-06-09 13:10:21 +1000

    Add support for visualizing langchain's async functions

--- langchain_visualizer/llms/base.py
+++ langchain_visualizer/llms/base.py
@@ -1,9 +1,12 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from ice.trace import add_fields
-from langchain.callbacks.manager import CallbackManagerForLLMRun, Callbacks
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForLLMRun,
+    CallbackManagerForLLMRun,
+    Callbacks,
+)
 from langchain.chat_models import ChatOpenAI
-from langchain.chat_models.base import BaseChatModel
 from langchain.llms.base import BaseLLM
 from langchain.llms.openai import OpenAI, OpenAIChat
 from langchain.schema import BaseMessage, ChatResult, LLMResult
@@ -17,6 +20,19 @@ MODEL_COST_MAP = {
 
 
 class LlmVisualizer(VisualizationWrapper):
+    def determine_cost(self, result: Union[LLMResult, ChatResult]) -> None:
+        llm_output = result.llm_output or {}
+        total_tokens = llm_output.get("token_usage", {}).get("total_tokens", 0)
+        davinci_equivalent = int(
+            MODEL_COST_MAP.get(self.og_obj.model_name, 0) * total_tokens
+        )
+        if davinci_equivalent > 0:
+            add_fields(davinci_equivalent_tokens=davinci_equivalent)
+
+
+class LlmSyncVisualizer(LlmVisualizer):
+    """Overrides the sync generate function for regular LLMs."""
+
     async def run(
         self,
         prompts: List[str],
@@ -28,17 +44,13 @@ class LlmVisualizer(VisualizationWrapper):
             self.og_obj, prompts=prompts, stop=stop, callbacks=callbacks
         )
         if isinstance(self.og_obj, OpenAI):
-            llm_output = result.llm_output or {}
-            total_tokens = llm_output.get("token_usage", {}).get("total_tokens", 0)
-            davinci_equivalent = int(
-                MODEL_COST_MAP.get(self.og_obj.model_name, 0) * total_tokens
-            )
-            if davinci_equivalent > 0:
-                add_fields(davinci_equivalent_tokens=davinci_equivalent)
+            self.determine_cost(result)
         return result
 
 
-class ChatLlmVisualizer(VisualizationWrapper):
+class ChatLlmSyncVisualizer(LlmVisualizer):
+    """Overrides the sync _generate function for chat LLMs."""
+
     async def run(
         self,
         messages: List[BaseMessage],
@@ -50,16 +62,31 @@ class ChatLlmVisualizer(VisualizationWrapper):
             self.og_obj, messages=messages, stop=stop, run_manager=run_manager
         )
         if isinstance(self.og_obj, OpenAIChat) or isinstance(self.og_obj, ChatOpenAI):
-            llm_output = result.llm_output or {}
-            total_tokens = llm_output.get("token_usage", {}).get("total_tokens", 0)
-            davinci_equivalent = int(
-                MODEL_COST_MAP.get(self.og_obj.model_name, 0) * total_tokens
-            )
-            if davinci_equivalent > 0:
-                add_fields(davinci_equivalent_tokens=davinci_equivalent)
+            self.determine_cost(result)
         return result
 
 
-ice_hijack(BaseLLM, "generate", LlmVisualizer)
-ice_hijack(BaseChatModel, "_generate", ChatLlmVisualizer)
-ice_hijack(ChatOpenAI, "_generate", ChatLlmVisualizer)
+class ChatLlmAsyncVisualizer(LlmVisualizer):
+    """Overrides the async _agenerate function for chat LLMs."""
+
+    async def _agenerate(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
+    ) -> ChatResult:
+        """Run the LLM on the given prompt and input."""
+        result: ChatResult = self.og_fn(
+            self.og_obj,
+            messages=messages,
+            stop=stop,
+            run_manager=run_manager,
+        )
+        if isinstance(self.og_obj, OpenAIChat) or isinstance(self.og_obj, ChatOpenAI):
+            self.determine_cost(result)
+        return result
+
+
+ice_hijack(BaseLLM, "generate", LlmSyncVisualizer)
+ice_hijack(ChatOpenAI, "_generate", ChatLlmSyncVisualizer)
+ice_hijack(ChatOpenAI, "_agenerate", ChatLlmAsyncVisualizer, is_async=True)
